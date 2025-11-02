19225c0c324947b56c36fdb500f288d93d854883
Enzo Soares <enzog2302@gmail.com>
2025-11-02 18:47:13 -0300

    Create __init__.py

---
+++ custom_samplers/__init__.py
@@ -0,0 +1,19 @@
+from custom_samplers.samplers import (
+    AdaptiveKernelSampler,
+    AdaptiveKernelSamplerConfig,
+    L0Sampler,
+    L0SamplerConfig,
+)
+from custom_samplers.model import (
+    CustomNerfModel,
+    CustomNerfModelConfig,
+)
+
+__all__ = [
+    "AdaptiveKernelSampler",
+    "AdaptiveKernelSamplerConfig",
+    "L0Sampler",
+    "L0SamplerConfig",
+    "CustomNerfModel",
+    "CustomNerfModelConfig",
+]
